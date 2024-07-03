f427e3a1276decca4896de43302be86c7dc37c9c
hemi <1830720944@qq.com>
2024-07-03 08:33:28 +0800

    feat: 使用uview2

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,45 +1,46 @@
 <template>
-  <view class="content">
-    <van-button type="primary">按钮</van-button>
-  </view>
+	<view class="content">
+		<u-button text="月落"></u-button>
+
+	</view>
 </template>
 
 <script>
-export default {
-  data() {
-    return {
-      title: "Hello",
-    };
-  },
-  onLoad() {},
-  methods: {},
-};
+	export default {
+		data() {
+			return {
+				title: "Hello",
+			};
+		},
+		onLoad() {},
+		methods: {},
+	};
 </script>
 
 <style>
-.content {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-}
+	.content {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
 
-.logo {
-  height: 200rpx;
-  width: 200rpx;
-  margin-top: 200rpx;
-  margin-left: auto;
-  margin-right: auto;
-  margin-bottom: 50rpx;
-}
+	.logo {
+		height: 200rpx;
+		width: 200rpx;
+		margin-top: 200rpx;
+		margin-left: auto;
+		margin-right: auto;
+		margin-bottom: 50rpx;
+	}
 
-.text-area {
-  display: flex;
-  justify-content: center;
-}
+	.text-area {
+		display: flex;
+		justify-content: center;
+	}
 
-.title {
-  font-size: 36rpx;
-  color: #8f8f94;
-}
+	.title {
+		font-size: 36rpx;
+		color: #8f8f94;
+	}
 </style>
